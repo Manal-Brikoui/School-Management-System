689f40c379ae6f174c38ab86e9dbd0f97795eecc
Manal Brikoui <manal.brikoui.ensao@ump.ac.ma>
2025-10-20 23:25:26 +0100

    Update Subject.cs

--- Domain/Entities/Subject.cs
+++ Domain/Entities/Subject.cs
@@ -8,7 +8,6 @@ namespace School.Domain.Entities
         public string Name { get; set; } = null!;
         public Guid TeacherId { get; set; }
 
-        // Propriété de navigation vers l'entité Teacher
         public Teacher? Teacher { get; set; }
     }
 }
